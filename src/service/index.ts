5c01177800fd2c77684c5430714c2d85a7c8997b
vkoziatnyk <vkoziatnyk@lemon-solutions.net>
2025-04-20 15:30:42 +0300

    add env API_SERVER

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,7 +1,9 @@
 class APIService {
+  baseURL = import.meta.env.API_SERVER || "https://plantin-server.onrender.com";
+
   async getTemp(): Promise<number | undefined> {
     try {
-      const res = await fetch("/api/temp");
+      const res = await fetch(`${this.baseURL}/api/temp`);
       const data: { temp: number } = await res.json();
       return data.temp;
     } catch (err: any) {
