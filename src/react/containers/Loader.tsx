70567e0aaa8cdf88b7080cea720f2bd195ab9fbd
vkoziatnyk <vkoziatnyk@lemon-solutions.net>
2025-04-20 16:00:41 +0300

    loader grid fixes

--- src/react/containers/Loader.tsx
+++ src/react/containers/Loader.tsx
@@ -6,7 +6,7 @@ const Loader = () => (
       <div className="row">
         <div className="news__category">Loading...Please stand by</div>
       </div>
-      <div className="grid grid-1 grid-3-md gap-24 gap-32-md">
+      <div className="grid grid-3 gap-24 gap-32-md">
         {[1, 2, 3].map(() => (
           <>
             <Skeleton />
